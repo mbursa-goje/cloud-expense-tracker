05201862782011d4e8279f1db8f31f1a0120dc46
Godwin Goje <godwingoje173@gmail.com>
2026-05-18 11:57:34 +0100

    chore(setup):  project folder structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-sm text-slate-900 font-bold'>
+        Hello
+      </h1>
+    </>
+  )
+}
+
+export default App
