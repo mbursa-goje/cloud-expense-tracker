fbcbba76d9f0517a06765334ddfa12c24f6f5950
Godwin Goje <godwingoje173@gmail.com>
2026-05-28 14:48:09 +0100

    feat: add protected route for dashboard pages

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -3,8 +3,12 @@ import { Navigate } from "react-router-dom";
 
 type ProtectedRouteProps = PropsWithChildren & {
     session: any; 
+    authReady: boolean;
 }
-export default function ProtectedRoute({children, session} : ProtectedRouteProps){
+export default function ProtectedRoute({children, session, authReady} : ProtectedRouteProps){
+    if(!authReady){
+        return null;
+    }
     if(!session){
         return <Navigate to="/auth/login"/>
     }
