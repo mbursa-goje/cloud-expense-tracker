eae0b263ade3d53a205ea31e99fde163393918ca
Godwin Goje <godwingoje173@gmail.com>
2026-06-02 09:31:17 +0100

    feat: implement dasboard overview page

---
+++ src/pages/dashboard/DashboardOverview.tsx
@@ -0,0 +1,8 @@
+import DashboardTable from '../../components/DashboardTable'
+
+export default function DashboardOverview(){
+    return <div className='p-3'>
+
+        <DashboardTable/>
+    </div>
+}
