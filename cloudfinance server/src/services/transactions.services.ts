7b3225cb14702988cc856c33704f023115915f65
Godwin Goje <godwingoje173@gmail.com>
2026-06-05 12:14:02 +0100

    feat: add new transaction row in recent transactions table

--- cloudfinance server/src/services/transactions.services.ts
+++ cloudfinance server/src/services/transactions.services.ts
@@ -1,20 +1,34 @@
-import { PrismaClient } from "../../generated/prisma/client.js"
-import { PrismaPg } from "@prisma/adapter-pg"
+import "dotenv/config";
+import { PrismaClient } from "../../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({ connectionString: process.env.DIRECT_URL})
-const prisma = new PrismaClient({adapter});
+console.log("DIRECT_URL:", process.env.DIRECT_URL);
+const adapter = new PrismaPg({ connectionString: process.env.DIRECT_URL });
+const prisma = new PrismaClient({ adapter });
 
-export const  createTransaction = async() => {
-    const transaction =  await prisma.transactions.create({
-        data: {
-            user_id: "e3c5d4d1-1234-5678-9012-abcdef123456",
-            description: "AWS EC2 Instance",
-            category: "Cloud",
-            amount: 25.99,
-            status: "pending",
-            name: "Monthly Server Cost"
-        },
-    });
+export const createTransaction = async () => {
+  const transaction = await prisma.transactions.create({
+    data: {
+      user_id: "b04370e8-e00f-4baa-960e-5eb0976bd84f",
+      description: "AWS EC2 Instance",
+      category: "database",
+      amount: 25.99,
+      status: "Pending",
+      name: "Monthly Server Cost",
+    },
+  });
 
-    return transaction;
-}
+
+  return transaction;
+};
+
+const run = async () => {
+  try {
+    const transaction = await createTransaction();
+    console.log("Success", transaction);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+run();
