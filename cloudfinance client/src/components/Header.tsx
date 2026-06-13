4f1560b591103b6646f855ce0a6356479b23bfc3
Godwin Goje <godwingoje173@gmail.com>
2026-06-13 21:12:45 +0100

    fix: fix type error

--- cloudfinance client/src/components/Header.tsx
+++ cloudfinance client/src/components/Header.tsx
@@ -2,14 +2,18 @@ import type { Session } from "@supabase/supabase-js";
 import { Badge } from "antd";
 import { Mail, Bell } from "lucide-react";
 import "../index.css";
-export default function Header({ session }: { session: Session }) {
+export default function Header({ session }: { session: Session | null }) {
   console.log("Session object", session);
   return (
-    <div className="h-12.5 md:pr-8 pr-4 justify-between items-center p-1 md:border-b hidden md:flex border-slate-200">
+    <div className="bg-[#fafafa] h-12.5 md:pr-8 pr-4 justify-between items-center p-1 md:border-b flex border-slate-200">
       {/* <div className="text-lg font-semibold text-(--primary)">
         {session?.user.user_metadata?.fullName ?? "User"}
       </div> */}
-      <div></div>
+      <div
+          className={`items-center text-center text-base md:pl-3  flex md:text-lg font-bold gap-1 text-(--primary) w-fit rounded-md border border-white/10 px-1 md:py-0.5`}
+        >
+          CloudFinance
+        </div>
       <div className="flex gap-2">
         <Badge color={"green"} count={1} size="small">
           <Mail size={20}></Mail>
